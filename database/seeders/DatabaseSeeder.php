5a4f90fc146a754cd7bef49a528eba7399650323
balvin22 <sb1181358@gmail.com>
2024-10-16 11:44:21 -0500

    backend-login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,11 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        //     'password'=>'123456789'
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password'=>'123456789'
+        ]);
         User::factory()->create([
             'name' => 'balvin',
             'email' => 'balvin@example.com',
